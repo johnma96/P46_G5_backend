5db2506f3082e7257a25803c36965b9e91b6284d
Laura Roa <lauramilena_92@hotmail.com>
2021-10-12 21:11:22 -0500

    G5P-9 Ajustes urls

--- authProject/urls.py
+++ authProject/urls.py
@@ -17,12 +17,8 @@ Including another URLconf
 from django.contrib                 import admin
 from django.urls                    import path
 from authApp                        import views  as authAppViews
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
-    path('admin/',                                  admin.site.urls),  # use defaul Djando Admin
-    path('login/',                                  TokenObtainPairView.as_view()), # use credentials to return tokens
-    path('refresh/',                                TokenRefreshView.as_view()), # generate new access token
-    path('user/',                                   authAppViews.UserCreateView.as_view()), # create a new user
-    path('user/<int:pk>/',                          authAppViews.UserDetailView.as_view()), # check info for an specific user based on id(pk)
+    path('admin/',                    admin.site.urls),  # use defaul Djando Admin
+    path('pruebas/create/',           authAppViews.PruebasCreateView.as_view()), # create a new test
 ]

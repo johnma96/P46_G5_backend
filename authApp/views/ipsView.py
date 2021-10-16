5d728e8dff330f924f17d66631410555ca273f10
Laura Roa <lauramilena_92@hotmail.com>
2021-10-15 20:34:29 -0500

    Insertar Ips y Departamento de forma exitosa

--- authApp/views/ipsView.py
+++ authApp/views/ipsView.py
@@ -2,7 +2,7 @@ from django.conf                                  import settings
 from rest_framework                               import views, status
 from rest_framework.response                      import Response
 from rest_framework.permissions                   import IsAuthenticated
-#from rest_framework_simplejwt.backends            import TokenBackend
+from rest_framework_simplejwt.backends            import TokenBackend
 
 from authApp.serializers.ipsSerializer import IpsSerializer
 
@@ -11,6 +11,15 @@ class IpsCreateView(views.APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
+
+        token        = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data   = tokenBackend.decode(token,verify=False)
+        
+        if valid_data['user_id'] != request.data['user_id']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+
         serializer = IpsSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()

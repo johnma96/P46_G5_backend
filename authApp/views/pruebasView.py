5e7859c4d114699f3b430fbc643aec13942aa449
John Montoya <jmmontoyaz@unal.edu.co>
2021-10-18 13:55:15 -0500

    Actualización y eliminación de pruebas exitosas

--- authApp/views/pruebasView.py
+++ authApp/views/pruebasView.py
@@ -1,4 +1,5 @@
 from django.conf                                  import settings
+from django.core.exceptions                       import PermissionDenied
 from rest_framework                               import generics, status
 from rest_framework.permissions                   import IsAuthenticated
 from rest_framework.response                      import Response
@@ -47,8 +48,9 @@ class PruebasDep_ipsView(generics.ListAPIView):
         valid_data   = tokenBackend.decode(token,verify=False)
         
         if valid_data['user_id'] != self.kwargs['user']:
-            stringResponse = {'detail':'Unauthorized Request'}
-            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+            raise PermissionDenied()
+            # stringResponse = {'detail':'Unauthorized Request'}
+            # return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
         
         queryset = Pruebas.objects.filter(dep_ips_id=self.kwargs['user'])
         return queryset
@@ -69,13 +71,15 @@ class PruebasDepartamentoView(generics.ListAPIView):
         
         #queryset = Pruebas.objects.select_related().filter(dep_ips__departamento__id = self.kwargs['user'])
         queryset = Pruebas.objects.select_related('dep_ips__departamento').filter(dep_ips__departamento__name = self.kwargs['departamento'])
-        print('*'*100)
-        print(str(queryset.query))
-        print('*'*100)
+        # print('*'*100)
+        # print(str(queryset.query))
+        # print('*'*100)
 
         return queryset
 
 class PruebasIpsView(generics.ListAPIView):
+    print('here')
+    print('*'*100)
     serializer_class = PruebasSerializer
     permission_classes = (IsAuthenticated,)
 
@@ -89,8 +93,44 @@ class PruebasIpsView(generics.ListAPIView):
         #     return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
 
         queryset = Pruebas.objects.select_related('dep_ips__ips').filter(dep_ips__ips__name = self.kwargs['ips'])
-        print('*'*100)
-        print(str(queryset.query))
-        print('*'*100)
+        # print('*'*100)
+        # print(str(queryset.query))
+        # print('*'*100)
 
         return queryset
+
+class PruebaUpdateView(generics.UpdateAPIView):
+    serializer_class = PruebasSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Pruebas.objects.all()
+    print(queryset, 'here'*100)
+
+    def update(self, request, *args, **kwargs):
+        token        = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data   = tokenBackend.decode(token,verify=False)
+
+        if valid_data['user_id'] != self.kwargs['user']:# or request.data['dep_ips'] != valid_data['user_id']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+
+        totalTests = request.data['positiveTests'] + request.data['negativeTests'] + request.data['indeterminateTests']
+        request.data['totalTests'] = totalTests
+
+        return super().update(request, *args, **kwargs)
+
+class PruebaDeleteView(generics.DestroyAPIView):
+    serializer_class = PruebasSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Pruebas.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        token        = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data   = tokenBackend.decode(token,verify=False)
+
+        if valid_data['user_id'] != self.kwargs['user']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+
+        return super().destroy(request, *args, **kwargs)
--- authProject/urls.py
+++ authProject/urls.py
@@ -5,18 +5,20 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 
 
 urlpatterns = [
-    path('admin/',                          admin.site.urls),  # use defaul Djando Admin
-    path('ips/',                            authAppViews.IpsCreateView.as_view())  , #create a new ips
-    path('departamento/',                   authAppViews.DepartamentoCreateView.as_view()), #create a new departamento
-    path('dep_ips/',                        authAppViews.Dep_ipsCreateView.as_view()), # create a new dep_ips
-    path('prueba/create/',                  authAppViews.PruebasCreateView.as_view()),
-    path('prueba/<int:user>/',              authAppViews.PruebasDep_ipsView.as_view()),
-    path('prueba/departamento/<str:departamento>/', authAppViews.PruebasDepartamentoView.as_view()),
-    path('prueba/ips/<str:ips>/',          authAppViews.PruebasIpsView.as_view()),
-    path('dep_ips/<int:pk>/',               authAppViews.Dep_ipsDetailView.as_view()), # check info for an specific dep_ips based on id(pk)
-    path('login/',                          TokenObtainPairView.as_view()), # use credentials to return tokens
-    path('refresh/',                        TokenRefreshView.as_view()), # generate new access token
-    path('ips/list/',            authAppViews.IpsListView.as_view()),
-    path('departamento/list/',   authAppViews.DepartamentoListView.as_view()),
+    path('admin/',                                  admin.site.urls),  # use defaul Djando Admin
+    path('ips/list/',                               authAppViews.IpsListView.as_view()), # get list of IPSs  
+    path('ips/create/',                             authAppViews.IpsCreateView.as_view())  , #create a new ips
+    path('departamento/list/',                      authAppViews.DepartamentoListView.as_view()),
+    path('departamento/create/',                    authAppViews.DepartamentoCreateView.as_view()), #create a new departamento    
+    path('dep_ips/',                                authAppViews.Dep_ipsCreateView.as_view()), # create a new dep_ips
+    path('dep_ips/<int:pk>/',                       authAppViews.Dep_ipsDetailView.as_view()), # check info for an specific dep_ips based on id(pk)   
+    path('prueba/create/',                          authAppViews.PruebasCreateView.as_view()), # create a new test for a specific dep_ips
+    path('prueba/<int:user>/',                      authAppViews.PruebasDep_ipsView.as_view()), # Filter test by dep_ips
+    path('prueba/ips/<str:ips>/',                   authAppViews.PruebasIpsView.as_view()),    #filter tests by IPS
+    path('prueba/departamento/<str:departamento>/', authAppViews.PruebasDepartamentoView.as_view()), #filter tests by departamento
+    path('prueba/update/<int:user>/<int:pk>/',      authAppViews.PruebaUpdateView.as_view()), # update a specific test
+    path('prueba/delete/<int:user>/<int:pk>/',      authAppViews.PruebaDeleteView.as_view()), # delete a specific test
+    path('login/',                                  TokenObtainPairView.as_view()), # use credentials to return tokens
+    path('refresh/',                                TokenRefreshView.as_view()), # generate new access token
 
 ]

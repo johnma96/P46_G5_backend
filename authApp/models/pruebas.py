c76ed3a8dcd9e8cdda26c32cffb65b066cc6d890
John Montoya <jmmontoyaz@unal.edu.co>
2021-10-18 13:54:08 -0500

    Actualizo valor por defecto del campo testDate

--- authApp/models/pruebas.py
+++ authApp/models/pruebas.py
@@ -1,10 +1,14 @@
-from django.db      import models
-from .dep_ips       import Dep_ips
+import django
+
+
+from .dep_ips              import Dep_ips
+from django.db             import models
+
 
 class Pruebas(models.Model):
     id                  = models.AutoField(primary_key=True)
     dep_ips             = models.ForeignKey(Dep_ips, related_name='pruebas', on_delete=models.CASCADE)
-    testDate            = models.DateTimeField()
+    testDate            = models.DateTimeField(default=django.utils.timezone.now)
     positiveTests       = models.IntegerField(default=0)
     negativeTests       = models.IntegerField(default=0)
     indeterminateTests  = models.IntegerField(default=0)
